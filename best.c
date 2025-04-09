dfc913bccedb4b474cc5870a8db356e8ac50ee2c
ushayenni <151708074+ushayenni@users.noreply.github.com>
2025-04-09 10:14:26 +0530

    Create best.c

---
+++ best.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+void BestFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+    for (int i = 0; i < n; i++)
+        allocation[i] = -1;
+    for (int i = 0; i < n; i++) {
+        int bestIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                if (bestIdx == -1 || blockSize[j] < blockSize[bestIdx])
+                    bestIdx = j;
+            }
+        }
+        if (bestIdx != -1) {
+            allocation[i] = bestIdx;
+            blockSize[bestIdx] -= processSize[i];
+        }
+    }
+    printf("Process No.\tProcess Size\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t", i, processSize[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i]);
+        else
+            printf("Not Allocated\n");
+    }
+}
+int main() {
+    int i, bs, p, blockSize[10], processSize[10];
+    printf("Enter no. of blocks: ");
+    scanf("%d", &bs);
+    for (i = 0; i < bs; i++) {
+        printf("Enter %d block size: ", i);
+        scanf("%d", &blockSize[i]);
+    }
+    printf("Enter no. of processes: ");
+    scanf("%d", &p);
+    for (i = 0; i < p; i++) {
+        printf("Enter %d process size: ", i);
+        scanf("%d", &processSize[i]);
+    }
+    BestFit(blockSize, bs, processSize, p);
+    return 0;
+}
